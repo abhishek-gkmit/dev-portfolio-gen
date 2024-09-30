b1b9746848e95c99f8bcfe852d1519f2ab301542
Abhishek <abhishek@gkmit.co>
2024-09-30 11:13:01 +0530

    fix: add `cloneObject` function to clone `developer` recursive implementation of `cloneObject`

--- src/api.ts
+++ src/api.ts
@@ -165,11 +165,33 @@ function cloneDeveloper(devId: number) {
     throw Error(`Error: Developer with id: ${devId} does not exist`);
   }
 
-  return {
-    ...developer,
-    skills: [...developer.skills],
-    projects: developer.projects.map((project) => ({ ...project })),
-  };
+  return cloneObject(developer);
+}
+
+function cloneObject(object: AnyObject): AnyObject {
+  if (object === null) {
+    return object;
+  }
+
+  if (Array.isArray(object)) {
+    let copy = [];
+    for (const value of object) {
+      copy.push(cloneObject(value));
+    }
+    console.log("second return");
+    return copy;
+  }
+
+  if (typeof object === "object") {
+    let copy: AnyObject = {};
+    for (const key of Object.keys(object)) {
+      copy[key] = cloneObject(object[key]);
+    }
+    console.log("third return");
+    return copy;
+  }
+
+  return object;
 }
 
 function validateProject(project: Project): boolean {
